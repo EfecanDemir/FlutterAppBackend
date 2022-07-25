e91fa0751c9901d9571d82a2d56d1975aebb47d4
EfecanDemir <demir.efecan@hotmail.com>
2022-07-25 21:55:51 +0300

    Add features

---
+++ Entities/Question.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entities
+{
+    public class Question
+    {
+        public int Id { get; set; }
+        public string Task { get; set; }
+        public int Point { get; set; }
+        public DateTime Enddate { get; set; }
+        public int CategoryId { get; set; }
+        public string Image { get; set; }
+        public string Description { get; set; }
+        public bool Status { get; set; }
+    }
+}
---
+++ Entities/Score.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entities
+{
+    public class Score
+    { 
+        public int Id { get; set; }
+        public int CouponId { get; set; }
+        public int UserTotalScore { get; set; }
+        public double UserTotalRate { get; set; }
+
+    }
+}
