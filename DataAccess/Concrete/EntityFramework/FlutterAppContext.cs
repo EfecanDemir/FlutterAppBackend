8ab98ad2518ff3c5fccb59257d3e530186e00b28
iremakalp1@hotmail.com <iremakalp1@hotmail.com>
2022-07-26 19:04:03 +0300

    DataAccess

---
+++ DataAccess/Concrete/EntityFramework/FlutterAppContext.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Core.Entities.Concrete;
+using Entities.Concrete;
+using Microsoft.EntityFrameworkCore;
+using Task = Entities.Concrete.Task;
+
+namespace DataAccess.Concrete.EntityFramework
+{
+    public class FlutterAppContext:DbContext
+    {
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Coupon> Coupons { get; set; }
+        public DbSet<Gift> Gifts { get; set; }
+        public DbSet<Order> Orders { get; set; }
+        public DbSet<Question> Questions { get; set; }
+        public DbSet<Rate> Rates { get; set; }
+        public DbSet<Task> Tasks { get; set; }
+        public DbSet<User> Users { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Server=(localdb)\mssqllocaldb;
+                        Database=FlutterAppDb");
+        }
+    }
+}
