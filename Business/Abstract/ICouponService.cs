7173af2e98dca84fff76d7c22a20a007fc0c72e3
EfecanDemir <demir.efecan@hotmail.com>
2022-07-27 16:18:20 +0300

    Service and Manager update

---
+++ Business/Abstract/ICouponService.cs
@@ -0,0 +1,19 @@
+﻿using Core.Utilities.Result;
+using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Business.Abstract
+{
+    public interface ICouponService
+    {
+        IDataResult<List<Coupon>> GetAll();
+        IResult Add(Coupon coupon);
+        IDataResult<List<Coupon>> GetByUserId(int userId);
+
+    }
+}
---
+++ Business/Concrete/CouponManager.cs
@@ -0,0 +1,39 @@
+﻿using Business.Abstract;
+using Core.Utilities.Result;
+using DataAccess.Abstract;
+using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.Concrete
+{
+    public class CouponManager : ICouponService
+    {
+        ICouponDal _couponDal;
+
+        public CouponManager(ICouponDal couponDal)
+        {
+            _couponDal = couponDal;
+        }
+
+        public IResult Add(Coupon coupon)
+        {
+            _couponDal.Add(coupon);
+            return new SuccessResult();
+        }
+
+        public IDataResult<List<Coupon>> GetAll()
+        {
+            return new SuccessDataResult<List<Coupon>>(_couponDal.GetAll());
+        }
+
+        public IDataResult<List<Coupon>> GetByUserId(int userId)
+        {
+            var result = _couponDal.GetAll(c => c.UserId == userId);
+            return new SuccessDataResult<List<Coupon>>(result);
+        }
+    }
+}
