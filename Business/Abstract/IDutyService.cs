2a8376d2cdec63b7601f2e75c03f7b3d08f2fe9a
iremakalp1@hotmail.com <iremakalp1@hotmail.com>
2022-07-27 16:19:33 +0300

    Business added

---
+++ Business/Abstract/IDutyService.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Core.Utilities.Result;
+using Entities.Concrete;
+
+namespace Business.Abstract
+{
+    public interface IDutyService
+    {
+        IDataResult<List<Duty>> GetAll();
+        IDataResult<List<Duty>> GetByCouponId(int couponId);
+    }
+}
