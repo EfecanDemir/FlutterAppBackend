9fb60861007706efe41cc0f2692611eb5e2f1e86
iremakalp1@hotmail.com <iremakalp1@hotmail.com>
2022-07-27 15:47:26 +0300

    Result added

---
+++ Entities/Concrete/Duty.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Core.Entities.Abstract;
+
+namespace Entities.Concrete
+{
+    public class Duty : IEntity
+    { 
+        public int Id { get; set; }
+        public int CouponId { get; set; }
+        public int RateId { get; set; }
+        public bool TaskStatus { get; set; }
+
+    }
+}
