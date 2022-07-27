31d3f80e208722d566e0774a1a97eb72539faeea
iremakalp1@hotmail.com <iremakalp1@hotmail.com>
2022-07-27 16:42:05 +0300

    Api controller added

---
+++ WebAPI/Controllers/DutiesController.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Business.Abstract;
+
+namespace WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DutiesController : ControllerBase
+    {
+        IDutyService _dutyService;
+        public DutiesController(IDutyService dutyService)
+        {
+            _dutyService = dutyService;
+        }
+
+        [HttpGet("getall")]
+        public IActionResult GetAll()
+        {
+            var result = _dutyService.GetAll();
+            if (result.Success)
+            {
+                return Ok(result);
+            }
+
+            return BadRequest(result);
+        }
+
+        [HttpGet("getbyid")]
+        public IActionResult GetByCouponId(int couponId)
+        {
+            var result = _dutyService.GetByCouponId(couponId);
+            if (result.Success)
+            {
+                return Ok(result);
+            }
+
+            return BadRequest(result);
+        }
+
+    }
+}
